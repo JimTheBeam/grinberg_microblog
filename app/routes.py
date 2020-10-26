2c435c72d58e3b9b5081c78f434c48c549e7d7b1
JimTheBeam <24dmitriy@mail.ru>
2020-10-26 22:36:21 +0300

    add language to posts, db migrate

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+
+from guess_language import guess_language
+
 from flask import g, flash, request, redirect, render_template, url_for
 from flask_babel import _, get_locale
 from flask_login import current_user, login_user, logout_user, login_required
@@ -25,7 +28,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live'))
