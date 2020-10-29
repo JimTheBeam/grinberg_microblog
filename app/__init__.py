da154537ab5a7715a91dda9780adefc6bdf78191
JimTheBeam <24dmitriy@mail.ru>
2020-10-30 00:14:31 +0300

    add search model with elasticsearch

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from elasticsearch import Elasticsearch
+
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -28,7 +30,7 @@ babel = Babel()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -37,6 +39,8 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
