d4007c20255b65467d3e7c4cef042402c060c95c
JimTheBeam <24dmitriy@mail.ru>
2020-10-30 02:18:47 +0300

    bugfix in full search

--- app/models.py
+++ app/models.py
@@ -18,13 +18,13 @@ class SearchableMixin(object):
             return cls.query.filter_by(id=0), 0
         when = []
         for i in range(len(ids)):
-            when.append((ids, i))
+            when.append((ids[i], i))
         return cls.query.filter(cls.id.in_(ids)).order_by(
             db.case(when, value=cls.id)), total
 
     @classmethod
     def before_commit(cls, session):
-        session.changes = {
+        session._changes = {
             'add': list(session.new),
             'update': list(session.dirty),
             'delete': list(session.deleted)
@@ -32,13 +32,13 @@ class SearchableMixin(object):
 
     @classmethod
     def after_commit(cls, session):
-        for obj in session.changes['add']:
+        for obj in session._changes['add']:
             if isinstance(obj, SearchableMixin):
                 add_to_index(obj.__tablename__, obj)
-        for obj in session.changes['update']:
+        for obj in session._changes['update']:
             if isinstance(obj, SearchableMixin):
                 add_to_index(obj.__tablename__, obj)
-        for obg in session.changes['delete']:
+        for obj in session._changes['delete']:
             if isinstance(obj, SearchableMixin):
                 remove_from_index(obj.__tablename__, obj)
         session._changes = None
@@ -46,12 +46,11 @@ class SearchableMixin(object):
     @classmethod
     def reindex(cls):
         for obj in cls.query:
-            add_to_index(cls._tablename__, obj)
+            add_to_index(cls.__tablename__, obj)
 
 
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
-    
 
 
 followers = db.Table('followers', 
