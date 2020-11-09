c34bf5f50a1f2b6c7f972df952b4f84a6e06077b
JimTheBeam <24dmitriy@mail.ru>
2020-11-09 21:56:57 +0300

    add export_posts

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,9 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
+from redis import Redis
+import rq
+
 from config import Config
 
 
@@ -41,6 +44,8 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
